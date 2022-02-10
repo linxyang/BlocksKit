0919d3b90b948bd6de1441086d1012eaa966c8f7
yanglx <1217789227@qq.com>
2022-02-10 16:47:37 +0800

    解决iOS 13及以上，系统表情键盘点击搜索框时卡死。

--- BlocksKit/DynamicDelegate/NSObject+A2BlockDelegate.m
+++ BlocksKit/DynamicDelegate/NSObject+A2BlockDelegate.m
@@ -332,6 +332,13 @@ typedef A2DynamicDelegate *(^A2GetDynamicDelegateBlock)(NSObject *, BOOL);
 		if ([delegate isEqual:dynamicDelegate]) {
 			delegate = nil;
 		}
+        // 解决iOS 13及以上，系统表情键盘点击搜索框时卡死。
+        if (@available(iOS 13.0,*)) {
+            if([delegate isKindOfClass:NSClassFromString(@"TUIEmojiSearchTextField")]) {
+                delegate = nil;
+            }
+        }
+        
 		dynamicDelegate.realDelegate = delegate;
 	});
 
